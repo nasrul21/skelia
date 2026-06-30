9799e6db5a64e2b6c9b1a2fce4c21829d527c590
Nasrul Faizin <faizinnasrul21@gmail.com>
2026-06-30 22:57:19 +0700

    Add forwarded refs to React components

---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @skelia/react
+
+## 0.1.1
+
+### Patch Changes
+
+- Add forwarded refs to React skeleton components
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,18 @@
-import type { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+import { forwardRef } from "react";
+import type {
+  ComponentPropsWithoutRef,
+  ComponentPropsWithRef,
+  ElementType,
+  ForwardedRef,
+  ForwardRefExoticComponent,
+  RefAttributes,
+  ReactElement,
+  ReactNode,
+} from "react";
 
 type SkeletonAnimation = "shimmer" | "pulse" | "wave" | "none";
+
+type PolymorphicRef<T extends ElementType> = ComponentPropsWithRef<T>["ref"];
 
 type SkeletonProps<T extends ElementType = "div"> = {
   as?: T;
@@ -9,54 +21,84 @@ type SkeletonProps<T extends ElementType = "div"> = {
   className?: string;
 } & Omit<ComponentPropsWithoutRef<T>, "as" | "className">;
 
+type SkeletonComponent = <T extends ElementType = "div">(
+  props: SkeletonProps<T> & { ref?: PolymorphicRef<T> },
+) => ReactElement | null;
+
+type SkeletonForwardRefComponent = ForwardRefExoticComponent<
+  SkeletonProps & RefAttributes<HTMLDivElement>
+> &
+  SkeletonComponent;
+
 function cx(...values: Array<string | false | null | undefined>) {
   return values.filter(Boolean).join(" ");
 }
 
-export function Skeleton<T extends ElementType = "div">({
-  as,
-  animation = "shimmer",
-  className,
-  children,
-  ...props
-}: SkeletonProps<T>) {
+function SkeletonRoot<T extends ElementType = "div">(
+  {
+    as,
+    animation = "shimmer",
+    className,
+    children,
+    ...props
+  }: SkeletonProps<T>,
+  ref: PolymorphicRef<T>,
+) {
   const Component = as ?? "div";
 
   return (
     <Component
+      ref={ref}
       className={cx("sk", `sk-${animation}`, className)}
       aria-hidden={props["aria-hidden" as keyof typeof props] ?? true}
       {...props}
     >
       {children}
     </Component>
-  )
+  );
 }
 
-export function SkeletonLine({
-  className,
-  ...props
-}: ComponentPropsWithoutRef<"div">) {
-  return <div className={cx("sk-line", className)} {...props} />
-}
+export const Skeleton = forwardRef(
+  SkeletonRoot as (
+    props: SkeletonProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => ReactElement | null,
+) as SkeletonForwardRefComponent;
 
-export function SkeletonBox({
-  className,
-  ...props
-}: ComponentPropsWithoutRef<"div">) {
-  return <div className={cx("sk-box", className)} {...props} />
-}
+Skeleton.displayName = "Skeleton";
 
-export function SkeletonCircle({
-  className,
-  ...props
-}: ComponentPropsWithoutRef<"div">) {
-  return <div className={cx("sk-circle", className)} {...props} />
-}
+export const SkeletonLine = forwardRef<
+  HTMLDivElement,
+  ComponentPropsWithoutRef<"div">
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cx("sk-line", className)} {...props} />
+));
 
-export function SkeletonAvatar({
-  className,
-  ...props
-}: ComponentPropsWithoutRef<"div">) {
-  return <div className={cx("sk-avatar", className)} {...props} />
-}
+SkeletonLine.displayName = "SkeletonLine";
+
+export const SkeletonBox = forwardRef<
+  HTMLDivElement,
+  ComponentPropsWithoutRef<"div">
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cx("sk-box", className)} {...props} />
+));
+
+SkeletonBox.displayName = "SkeletonBox";
+
+export const SkeletonCircle = forwardRef<
+  HTMLDivElement,
+  ComponentPropsWithoutRef<"div">
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cx("sk-circle", className)} {...props} />
+));
+
+SkeletonCircle.displayName = "SkeletonCircle";
+
+export const SkeletonAvatar = forwardRef<
+  HTMLDivElement,
+  ComponentPropsWithoutRef<"div">
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cx("sk-avatar", className)} {...props} />
+));
+
+SkeletonAvatar.displayName = "SkeletonAvatar";
